ea11cf691a4fbb6b0f2f65d784c3b4b11beecfad
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-03 08:11:27 -0500

    corrected variable name

--- twoOneMultiplexer_tb.v
+++ twoOneMultiplexer_tb.v
@@ -1,4 +1,24 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/03/2026 08:05:09 AM
+// Design Name: 
+// Module Name: twoOneMultiplexer_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module twoOneMultiplexer_tb();
     reg I0, I1, S, E;
@@ -11,24 +31,24 @@ module twoOneMultiplexer_tb();
         E = 1'b0;
         I0 = 1'b0;
         I1 = 1'b1;
-        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,E,Y,I0,I1);
         
         #10 S = 1'b0;
         E = 1'b0;
         I0 = 1'b0;
         I1 = 1'b1;
-        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,E,Y,I0,I1);
         
         #10 S = 1'b1;
         E = 1'b1;
         I0 = 1'b0;
         I1 = 1'b1;
-        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,E,Y,I0,I1);
         
         #10 S = 1'b0;
         E = 1'b1;
         I0 = 1'b0;
         I1 = 1'b1;
-        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,E,Y,I0,I1);
     end
 endmodule
