83a98f126fae588a8223a21e04cefd0fb3d63352
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-03 08:16:48 -0500

    change variable declaration size

--- twoOneMultiplexer_tb.v
+++ twoOneMultiplexer_tb.v
@@ -1,28 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/03/2026 08:05:09 AM
-// Design Name: 
-// Module Name: twoOneMultiplexer_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module twoOneMultiplexer_tb();
-    reg I0, I1, S, E;
-    wire Y;
+    reg[1:0] I0, I1;
+    reg S, E;
+    wire[1:0] Y;
     
     twoOneMultiplexer uut(I0,I1,S,E,Y);
     
