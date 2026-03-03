a2bb869774eedd646a08ba8fc531b8a3d3faaa04
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-03 08:19:52 -0500

    revert back because Boolean logic was breaking

--- twoOneMultiplexer_tb.v
+++ twoOneMultiplexer_tb.v
@@ -1,17 +1,16 @@
 `timescale 1ns / 1ps
 
 module twoOneMultiplexer_tb();
-    reg[1:0] I0, I1;
-    reg S, E;
-    wire[1:0] Y;
+    reg I0, I1, S, E;
+    wire Y;
     
     twoOneMultiplexer uut(I0,I1,S,E,Y);
     
     initial begin
         #10 S = 1'b1;
         E = 1'b0;
-        I0 = 1'b1;
-        I1 = 2'b10;
+        I0 = 1'b0;
+        I1 = 1'b1;
         #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,E,Y,I0,I1);
         
         #10 S = 1'b0;
