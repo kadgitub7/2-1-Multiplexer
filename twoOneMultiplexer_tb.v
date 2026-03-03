113c2e24020d28603dc331a3c5826f4cce7b8123
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-03 08:10:02 -0500

    created testbench

---
+++ twoOneMultiplexer_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module twoOneMultiplexer_tb();
+    reg I0, I1, S, E;
+    wire Y;
+    
+    twoOneMultiplexer uut(I0,I1,S,E,Y);
+    
+    initial begin
+        #10 S = 1'b1;
+        E = 1'b0;
+        I0 = 1'b0;
+        I1 = 1'b1;
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        
+        #10 S = 1'b0;
+        E = 1'b0;
+        I0 = 1'b0;
+        I1 = 1'b1;
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        
+        #10 S = 1'b1;
+        E = 1'b1;
+        I0 = 1'b0;
+        I1 = 1'b1;
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+        
+        #10 S = 1'b0;
+        E = 1'b1;
+        I0 = 1'b0;
+        I1 = 1'b1;
+        #10 $display("S = %b, E = %b, Y = %b, I0 = %b, I1 = %b", S,B,Y,I0,I1);
+    end
+endmodule
